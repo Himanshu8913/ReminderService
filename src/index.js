80a0b3d318804483991fd368f551dd90ad130728
Himanshu Gupta <himanshugupta558@gmail.com>
2023-03-07 12:48:07 +0530

    Testing node-cron to send mail/reminder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+// const cron = require('node-cron');
 
-const { sendBasicEmail } = require('./services/email-service');
+// const { sendBasicEmail } = require('./services/email-service');
 const { PORT } = require('./config/serverConfig');
 
 const setupAndStartServer = () => {
@@ -12,12 +13,15 @@ const setupAndStartServer = () => {
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);   
         
-        sendBasicEmail(
-            '"Support" <support@admin.com>',
-            'himanshugupta8913@gmail.com',
-            'This is a testing email',
-            'Hey, how are you, I hope you like the support'
-        );
+        // sendBasicEmail(
+        //     '"Support" <support@admin.com>',
+        //     'himanshugupta8913@gmail.com',
+        //     'This is a testing email',
+        //     'Hey, how are you, I hope you like the support'
+        // );
+        // cron.schedule('*/2 * * * *', () => {
+        //     console.log('Running a task every 2 minutes');
+        // });
     });
 }
 
